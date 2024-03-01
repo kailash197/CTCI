ac4c88940cbf67087deaf09b2f11d2fb9d78aaee
Kailash Khadka <k.khadka343@gmail.com>
2024-03-01 14:15:46 -0500

    1-6 Basic String Compression

---
+++ test.cpp
@@ -0,0 +1,16 @@
+/*
+ * Cracking the coding interview edition 6
+ * Problem 1-6 Implement a method to perform basic string compression.
+ * Example string aabcccccaaa should be compressed to a2b1c5a3,
+ * however if compressed string is bigger than original string, return original string
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    return 0;
+}
